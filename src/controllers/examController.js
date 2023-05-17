982bb7d668a7d52237b94a85ecba69777ea225d9
Assami Muzaki <muzakiassami@gmail.com>
2023-05-17 16:37:10 +0700

    complete input function on upload soal

--- src/controllers/examController.js
+++ src/controllers/examController.js
@@ -14,49 +14,51 @@ Exam.hasMany(Question);
 Question.belongsTo(Exam);
 
 async function tambahExam(req, res) {
-  const {
-    exam_type,
-    kkm_point,
-    available_try,
-    question_text,
-    answer,
-    wrong_answer1,
-    wrong_answer2,
-    wrong_answer3,
-  } = req.body;
+  console.log(req.body);
+  res.redirect("/ujian");
+  // const {
+  //   exam_type,
+  //   kkm_point,
+  //   available_try,
+  //   question_text,
+  //   answer,
+  //   wrong_answer1,
+  //   wrong_answer2,
+  //   wrong_answer3,
+  // } = req.body;
 
-  const score = await Score.findAll();
-  const exam = await Exam.create({
-    exam_type,
-    kkm_point,
-    available_try,
-  });
+  // const score = await Score.findAll();
+  // const exam = await Exam.create({
+  //   exam_type,
+  //   kkm_point,
+  //   available_try,
+  // });
 
-  if (Array.isArray(question_text)) {
-    question_text.forEach(async (qt, index) => {
-      const question = await Question.create({
-        question_text: question_text[index],
-        answer: answer[index],
-        wrong_answer1: wrong_answer1[index],
-        wrong_answer2: wrong_answer2[index],
-        wrong_answer3: wrong_answer3[index],
-      });
-      exam.addQuestions(question);
-      exam.addScores(score);
-    });
-    return res.redirect("http://localhost:3000/exams");
-  } else {
-    const question = await Question.create({
-      question_text,
-      answer,
-      wrong_answer1,
-      wrong_answer2,
-      wrong_answer3,
-    });
-    exam.addQuestions(question);
-    exam.addScores(score);
-    return res.redirect("http://localhost:3000/exams");
-  }
+  // if (Array.isArray(question_text)) {
+  //   question_text.forEach(async (qt, index) => {
+  //     const question = await Question.create({
+  //       question_text: question_text[index],
+  //       answer: answer[index],
+  //       wrong_answer1: wrong_answer1[index],
+  //       wrong_answer2: wrong_answer2[index],
+  //       wrong_answer3: wrong_answer3[index],
+  //     });
+  //     exam.addQuestions(question);
+  //     exam.addScores(score);
+  //   });
+  //   return res.redirect("http://localhost:3000/exams");
+  // } else {
+  //   const question = await Question.create({
+  //     question_text,
+  //     answer,
+  //     wrong_answer1,
+  //     wrong_answer2,
+  //     wrong_answer3,
+  //   });
+  //   exam.addQuestions(question);
+  //   exam.addScores(score);
+  //   return res.redirect("http://localhost:3000/exams");
+  // }
 }
 
 async function getAllExam(req, res, next) {
