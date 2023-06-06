9f13c7738912d20ceb5d95aac159de2dbb9c0b08
Assami Muzaki <muzakiassami@gmail.com>
2023-06-06 11:32:32 +0700

    complete create exam

--- public/js/examCreate.js
+++ public/js/examCreate.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
         <div class="questions-box">
           <div class="questions">
             <div class="question">
-            <p>Soal</p>
+              <p>Soal</p>
               <div class="display_image"></div>
               <textarea name="question_text" class='soal-text' placeholder="Masukan Soal"></textarea>
               <div class="answers">
@@ -27,6 +27,9 @@ $(document).ready(() => {
                     <input type="file" class="input-file" multiple="multiple" name="question_img" accept="image/*"/>
                     <i class="uil uil-file-plus-alt"></i> Masukan Gambar
                 </label>
+              </div>
+              <div class="delete-quest" title="Hapus Soal">
+                <span><i class="uil uil-trash-alt"></i></span>
               </div>
             </div>
           </div>
@@ -51,22 +54,28 @@ $(document).ready(() => {
             <div class="display_image"></div>
             <textarea name="question_text" class='soal-text' placeholder="Masukan Soal"></textarea>
             <div class="answers">
-              <input placeholder='jawaban benar' name='correct_answer'required class='answer'/>
-              <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
-              <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
-              <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
-              <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
-              
-              <label class="custom-file-upload">
-                    <input type="file" class="input-file" multiple="multiple" name="question_img" accept="image/*"/>
-                    <i class="uil uil-file-plus-alt"></i> Masukan Gambar
-              </label>
+                <input placeholder='jawaban benar' name='correct_answer'required class='answer'/>
+                <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
+                <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
+                <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
+                <input placeholder='jawaban lain' name='wrong_answer'required class='answer'/>
+                
+                <label class="custom-file-upload">
+                      <input type="file" class="input-file" multiple="multiple" name="question_img" accept="image/*"/>
+                      <i class="uil uil-file-plus-alt"></i> Masukan Gambar
+                </label>
+            </div>
+            <div class="delete-quest" title="Hapus Soal" >
+              <span><i class="uil uil-trash-alt"></i></span>
             </div>
           </div>
         `,
     ]);
   });
 
+  $(".main-background").on("click", ".delete-quest", function () {
+    $(this).parent().remove();
+  });
   $(".main-background").on("click", "#selesai", () => {
     $(".submit-layer").css("visibility", "visible");
   });
@@ -83,10 +92,16 @@ $(document).ready(() => {
     let input_file = document.querySelectorAll(".input-file");
     queuedImagesArray = [];
     input_file.forEach((inp, index) => {
-      if (input_file[index].files[0].size < 200000) {
+      if (input_file[index].files[0] == undefined) {
         queuedImagesArray.push(input_file[index].files);
-      } else {
-        $(".file-layer").css("visibility", "visible");
+      } else if (input_file[index].files[0]) {
+        if (input_file[index].files[0].size < 200000) {
+          queuedImagesArray.push(input_file[index].files);
+          document.querySelectorAll(".display_image")[index].style.display =
+            "flex";
+        } else {
+          $(".file-layer").css("visibility", "visible");
+        }
       }
     });
     displayQueuedImages();
@@ -100,7 +115,7 @@ $(document).ready(() => {
       if (image.length != 0) {
         img = `
         <img src="${URL.createObjectURL(image[0])}" alt="no img" />
-        <span class="deleteImg">&times;</span>
+        <span title="Hapus Gambar" class="deleteImg"><i class="uil uil-times"></i></span>
         `;
       } else {
         img = "";
@@ -111,9 +126,13 @@ $(document).ready(() => {
 
   $(".main-background").on("click", ".deleteImg", function (e) {
     let input_file = document.querySelectorAll(".input-file");
-    let index = $(".deleteImg").index($(this));
+    let index = $(".display_image").index($(this).parent());
     input_file[index].type = "text";
     input_file[index].type = "file";
     $(this)[0].parentElement.innerHTML = "";
+    document.querySelectorAll(".display_image")[index].style.display = "none";
+    // console.log($(".display_image"));
+    // console.log($(this).parent());
+    // console.log($(".display_image").index($(this).parent()));
   });
 });
