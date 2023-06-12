a6a5d0b6daaeda37d9008b50398f1e895a890f84
Assami Muzaki <muzakiassami@gmail.com>
2023-06-12 19:36:21 +0700

    fix nilai page

--- public/js/nilaiPage.js
+++ public/js/nilaiPage.js
@@ -6,74 +6,13 @@ $(document).ready(() => {
   });
   $("#date").html(text);
 
-  // CALL
+  let exams;
 
-  const url = "/api/scores";
-  let datas;
-  $.get(url, async (data, status) => {
-    if (status == "success" && data.payload.datas.length !== 0) {
-      let allData = data.payload.datas;
-      $("#siswa-table").DataTable({
-        ajax: {
-          url: "/api/scores",
-          dataSrc: "payload.datas",
-        },
-        columns: [
-          {
-            data: null,
-            render: function (data, type, row, meta) {
-              return meta.row + meta.settings._iDisplayStart + 1;
-            },
-          },
-          { data: "nis" },
-          { data: "username" },
-          {
-            data: "Exams",
-            render: function (data) {
-              if (data.length !== 0) {
-                let correct = data.filter((e) => {
-                  return e.unique_id == $("#exams-select").val();
-                });
-                if (correct.length !== 0) {
-                  return correct[0].exam_name;
-                }
-                return "Belum mengambil ujian";
-              } else {
-                return "Belum mengambil ujian";
-              }
-            },
-          },
-          {
-            data: "Exams",
-            render: function (data) {
-              if (data.length !== 0) {
-                let correct = data.filter((e) => {
-                  return e.unique_id == $("#exams-select").val();
-                });
-                if (correct.length !== 0) {
-                  return correct[0].ScoreExam.point;
-                }
-                return "0";
-              } else {
-                return "0";
-              }
-            },
-          },
-        ],
-      });
-    } else {
-      $(".main-table-body").append([
-        `
-                <img src="/img/nothing.png" alt="" />
-                <p>Belum ada nilai</p>
-              `,
-      ]);
-    }
-  });
+  // CALL
 
   $.get("/api/exams", async (response, status) => {
     $("#exams-select").html("");
-    let exams = response.payload.datas;
+    exams = response.payload.datas;
     if (exams.length !== 0) {
       exams.forEach((exam, index) => {
         $("#exams-select").append(
@@ -84,6 +23,70 @@ $(document).ready(() => {
       $("#exams-select").append(
         `<option value="">tidak ada ujian terdaftar</option>`
       );
+    }
+  });
+
+  const url = "/api/scores";
+  let datas;
+  $.get(url, async (data, status) => {
+    if (status == "success" && data.payload.datas.length !== 0) {
+      if (exams.length !== 0) {
+        $("#siswa-table").DataTable({
+          ajax: {
+            url: "/api/scores",
+            dataSrc: "payload.datas",
+          },
+          columns: [
+            {
+              data: null,
+              render: function (data, type, row, meta) {
+                return meta.row + meta.settings._iDisplayStart + 1;
+              },
+            },
+            { data: "nis" },
+            { data: "username" },
+            {
+              data: "Exams",
+              render: function (data) {
+                if (data.length !== 0) {
+                  let correct = data.filter((e) => {
+                    return e.unique_id == $("#exams-select").val();
+                  });
+                  if (correct.length !== 0) {
+                    return correct[0].exam_name;
+                  }
+                  return "Belum mengambil ujian";
+                } else {
+                  return "Belum mengambil ujian";
+                }
+              },
+            },
+            {
+              data: "Exams",
+              render: function (data) {
+                if (data.length !== 0) {
+                  let correct = data.filter((e) => {
+                    return e.unique_id == $("#exams-select").val();
+                  });
+                  if (correct.length !== 0) {
+                    return correct[0].ScoreExam.point;
+                  }
+                  return "0";
+                } else {
+                  return "0";
+                }
+              },
+            },
+          ],
+        });
+      } else {
+        $(".main-table-body").append([
+          `
+                  <img src="/img/nothing.png" alt="" />
+                  <p>Belum ada nilai</p>
+                `,
+        ]);
+      }
     }
   });
 
